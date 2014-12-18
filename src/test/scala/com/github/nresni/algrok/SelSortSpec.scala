ef867395d56f1f2bb4b212815437312c15bded9c
nresni <nresnikow@gmail.com>
2014-12-18 22:33:21 +0100

    format

--- src/test/scala/com/github/nresni/algrok/SelSortSpec.scala
+++ src/test/scala/com/github/nresni/algrok/SelSortSpec.scala
@@ -20,41 +20,25 @@ class SelSortSpec extends FunSpec with Matchers {
 
   describe("Selection sort of  Array(2, 1, 3)") {
     it("should equals Array(1, 2, 3)") {
-      SelSort
-        .selectionSortBasic(
-          Array(2, 1,
-            3)) should
-        equal(
-          Array(1, 2,
-            3))
+      SelSort.selectionSortBasic(Array(2, 1, 3)) should equal(Array(1, 2, 3))
     }
   }
 
   describe("SmallestRec of  Array(1, 2, 3)") {
     it("should equals 0") {
-      SelSort.findSmallestBasicRec(
-        Array(1, 2, 3)) should
-        be(0)
+      SelSort.findSmallestBasicRec(Array(1, 2, 3)) should be(0)
     }
   }
 
   describe("SmallestRec of  Array(2, 1, 3)") {
     it("should equals 1") {
-      SelSort.findSmallestBasicRec(
-        Array(2, 1, 3)) should
-        be(1)
+      SelSort.findSmallestBasicRec(Array(2, 1, 3)) should be(1)
     }
   }
 
   describe("Selection sort using rec of  Array(2, 1, 3)") {
     it("should equals 0") {
-      SelSort
-        .selectionSortBasicUsingRec(
-          Array(2, 1,
-            3)) should
-        equal(
-          Array(1, 2,
-            3))
+      SelSort.selectionSortBasicUsingRec(Array(2, 1, 3)) should equal(Array(1, 2, 3))
     }
   }
 }
